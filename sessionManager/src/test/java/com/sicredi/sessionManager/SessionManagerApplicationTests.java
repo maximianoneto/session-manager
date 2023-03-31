25982f68f0adc11027921c61ae57ec8951c34e03
Maximianocn <maximiano.neto@sou.unifal-mg.edu.br>
2023-03-31 14:53:30 -0300

    Classes de Teste Unitários da aplicação

---
+++ sessionManager/src/test/java/com/sicredi/sessionManager/SessionManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sicredi.sessionManager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SessionManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
